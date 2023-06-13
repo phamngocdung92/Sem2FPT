c598b8c302b96f3a6842109a9322e59f5ed42940
PhamNgocDung92 <130581370+phamngocdung92@users.noreply.github.com>
2023-06-13 10:04:35 +0700

    Delete app directory

--- app/routers/account.login.router.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app){
-    var userController = require("../controllers/user.controller"); // controller
-    var middleware = require("../commons/Author_middleware");
-
-    app.post("/account/login", userController.login);
-    app.post("/account/create", userController.add_user);
-    app.post("/account/refresh", userController.refreshToken);
-    app.post("/account/logout", middleware.verifyToken, userController.logout);
-}
