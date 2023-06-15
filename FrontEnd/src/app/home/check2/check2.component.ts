10a231caabff398d9806858d65686d6123fb0270
Viet Anh <dovietanh2k@gmail.com>
2023-06-15 11:15:09 +0700

    commit sua lai xoa di 2 files cu

--- FrontEnd/src/app/home/check2/check2.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'app-check2',
-  templateUrl: './check2.component.html',
-  styleUrls: ['./check2.component.css']
-})
-export class Check2Component {
-}
--- FrontEnd/src/polyfills.ts
+++
@@ -1,4 +0,0 @@
-import 'zone.js/dist/zone';
-import 'core-js/es/reflect';
-import 'core-js/es/reflect';
-import '@angular/localize/init';
